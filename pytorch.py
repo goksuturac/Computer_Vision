190cd61d2f065cd2855c8d865842f71496010668
Göksu Turaç <turacgoksu@gmail.com>
2024-02-10 18:14:48 +0300

    commit to main

--- pytorch.py
+++ pytorch.py
@@ -1,4 +1,6 @@
 import torch
+# In practice, you'll often see scalars and vectors denoted as lowercase letters such as y or a.
+# And matrices and tensors denoted as uppercase letters such as X or W.
 
 scalar = torch.tensor(7)
 print(scalar.ndim)
@@ -8,11 +10,12 @@ vector = torch.tensor([7,7])
 print(vector.ndim)
 print(vector.shape)
 
-matrix = torch.tensor([[7,7],[8,8]])
-print(matrix.ndim)
-print(matrix.shape)
+MATRIX = torch.tensor([[7, 7], [8, 8]])
+print(MATRIX.ndim)
+print(MATRIX.shape)
 
 
-tensor = torch.tensor([[[1,2,3],[4,5,6],[7,8,9]]])
-print(tensor.ndim)
-print(tensor.shape)
+TENSOR = torch.tensor([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
+print(TENSOR.ndim)
+print(TENSOR.shape)
+
